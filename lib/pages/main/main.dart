c3270cc6673e4bebdefd76cc8929b8d97b682806
andreaji <andrea.hb.ji@outlook.com>
2021-12-17 21:58:31 +0800

    add -> 06 代码规范、业务代码组织、新闻首页实现

--- lib/pages/main/main.dart
+++ lib/pages/main/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter_ducafecat_news/common/apis/apis.dart';
 import 'package:flutter_ducafecat_news/common/entitys/entitys.dart';
 import 'package:flutter_ducafecat_news/common/utils/utils.dart';
 import 'package:flutter_ducafecat_news/common/values/values.dart';
+import 'package:flutter_ducafecat_news/pages/main/ad_widget.dart';
 import 'package:flutter_ducafecat_news/pages/main/categories_widget.dart';
+import 'package:flutter_ducafecat_news/pages/main/channels_widget.dart';
+import 'package:flutter_ducafecat_news/pages/main/news_item_widget.dart';
+import 'package:flutter_ducafecat_news/pages/main/newsletter_widget.dart';
+import 'package:flutter_ducafecat_news/pages/main/recommend_widget.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key key}) : super(key: key);
@@ -42,15 +47,15 @@ class _MainPageState extends State<MainPage> {
 
   // 分类菜单
   Widget _buildCategories() {
-    return newsCategoriesWidget(
-      categories: _categories,
-      selCategoryCode: _selCategoryCode,
-      onTap: (CategoryResponseEntity item) {
-        setState(() {
-          _selCategoryCode = item.code;
-        });
-      },
-    );
+    return _categories == null
+        ? Container()
+        : newsCategoriesWidget(
+            categories: _categories,
+            selCategoryCode: _selCategoryCode,
+            onTap: (CategoryResponseEntity item) {
+              // _loadNewsData(item.code);
+            },
+          );
   }
   // 抽取前先实现业务
   // Widget _buildCategories() {
@@ -88,35 +93,56 @@ class _MainPageState extends State<MainPage> {
 
   // 推荐阅读
   Widget _buildRecommend() {
-    return Container(
-      height: duSetHeight(490),
-      color: Colors.amber,
-    );
+    return _newsRecommend == null // 数据没到位，可以用骨架图展示
+        ? Container()
+        : recommendWidget(_newsRecommend);
   }
 
   // 频道
   Widget _buildChannels() {
-    return Container(
-      height: duSetHeight(137),
-      color: Colors.blueAccent,
-    );
+    return _channels == null
+        ? Container()
+        : newsChannelsWidget(
+            channels: _channels,
+            onTap: (ChannelResponseEntity item) {},
+          );
   }
 
   // 新闻列表
   Widget _buildNewsList() {
-    return Container(
-      height: duSetHeight(161 * 5 + 100.0),
-      color: Colors.purple,
-    );
+    return _newsPageList == null
+        ? Container(
+            height: duSetHeight(161 * 5 + 100.0),
+          )
+        : Column(
+            children: _newsPageList.items.map((item) {
+              // 新闻行
+              List<Widget> widgets = <Widget>[
+                newsItem(item),
+                Divider(height: 1),
+              ];
+
+              // 每 5 条 显示广告
+              int index = _newsPageList.items.indexOf(item);
+              if (((index + 1) % 5) == 0) {
+                widgets.addAll(<Widget>[
+                  adWidget(),
+                  Divider(height: 1),
+                ]);
+              }
+
+              // 返回
+              return Column(
+                children: widgets,
+              );
+            }).toList(),
+          );
   }
 
   // ad 广告条
   // 邮件订阅
   Widget _buildEmailSubscribe() {
-    return Container(
-      height: duSetHeight(259),
-      color: Colors.brown,
-    );
+    return newsletterWidget();
   }
 
   @override
